85eaf6ce45c6a0b0a88b21b2cc25771bed022e35
ccanovi <camilla.canovi@umu.se>
2019-06-19 18:11:41 +0200

    functioning shiny app

--- App.R
+++ App.R
@@ -1,26 +1,35 @@
 library(shiny)
 library(ggplot2)
 library(gganimate)
+library(tidyverse)
+library(data.table)
+
 shinyApp(
   ui=fluidPage(
     selectInput(inputId = "selectVariable", label = "Select the orchid",
                 choices = c("family","genus","species")),
     imageOutput("output_plot")
-  ),
+    ),
   
   server = function(input, output) {
     fn_plot <- reactive({ 
-      #validate(need(input$orchidsgotland.csv), "Please select an option")
-      orchids<-as_tibble(fread("data/orchids_gotland/orchidsgotland.csv"))
+      orchids <-as_tibble(fread("data/orchids_gotland/orchidsgotland.csv"))
+      outfile <- tempfile(fileext='.gif')
       p1 <- orchids %>% ggplot(aes(decimalLongitude, decimalLatitude)) +
         geom_point(aes(col = input$selectVariable), alpha = 0.4)
       anim <- p1 + 
         transition_states(year,
                           transition_length = 2,
                           state_length = 1)
-    return(anim)
-    })
-    
-    output$output_plot <- renderPlot(fn_plot())
+      
+      # save
+      anim_save("outfile.gif", animate(anim)) 
+      # return a list
+      list(src = "outfile.gif",
+           contentType = 'image/gif'
+         
+      )})
+    #})
+    output$output_plot <- renderImage(fn_plot())
      
   })
