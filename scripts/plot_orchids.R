a8bb6a17c165faea038db409d196586b348b0806
Karen Lykkebo <karenlykkebo@gmail.com>
2019-06-19 13:40:19 +0200

    added animation

--- scripts/plot_orchids.R
+++ scripts/plot_orchids.R
@@ -4,24 +4,33 @@ library(tidyverse)
 library(ggmap)
 library(sf)
 library(Hmisc)
+library(gganimate)
 
 gotland <- st_read("gotland.shp")
 orchids <- fread("./data/orchids_gotland/orchidsgotland.csv")
+
+orchids <- orchids %>% 
+  rename("lat" = "decimalLatitude",
+         "long" = "decimalLongitude")
 
 # Write small function that maps gotland and adds data with long, lat and observations.
 orchidsmap <- function(my_df, x1, y1, z1) {
   
   ggplot(my_df) +
     geom_sf(data = gotland) +
-    geom_point(aes_string(x1, y1, col = z1)) +
+    geom_point(aes_string(x1, y1, col = z1), size = 3, alpha = 0.8) +
     theme_bw() +
-    labs(x = "Longitude", y = "Latitude", col = capitalize(z1)) 
+    labs(x = "Longitude", y = "Latitude", col = capitalize(z1))
+    
 }
 
-
 orchids %>% 
-  filter(year == 2015) %>% 
-  orchidsmap("decimalLongitude", "decimalLatitude", "species")
+orchidsmap("long", "lat", "genus") +
+  transition_states(year, 
+                    transition_length = 0.5,
+                    state_length = 1)
+
+
 
 
 
